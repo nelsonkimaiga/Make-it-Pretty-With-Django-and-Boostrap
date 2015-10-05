ad2959772c3fe6ce02a9e18527b10f310e27b766
Nelson Kimaiga <nelsonkimaiga@aol.com>
2015-10-05 14:55:05 +0300

    Form Validation added

--- myApp/forms.py
+++ myApp/forms.py
@@ -9,7 +9,9 @@ class StudentForm(forms.ModelForm):
 
 	def clean_age(self):
 	    age = self.clean_data.get('age')
-	    if age > 120;
-           raise forms.ValidationError("You may be too old for this class")	
+	    if age > 120:
+           raise forms.ValidationError("You may be too old for this class")
+        elif age < 10: 
+        raise forms.ValidationError("You may be too young for this class") 
 	    return age	
 
