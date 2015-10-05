072a379319139db0d08878f4a83fee1281a90f55
Nelson Kimaiga <nelsonkimaiga@aol.com>
2015-10-05 12:52:47 +0300

    Models Created

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+	fullname = models.CharField(max_length=200)
+	age = models.IntegerField()
+	email = models.EmailField()
+	interests = models.TextField()
+	registered_date = models.DateField(auto_now=False, auto_now_add=True)
+	last_update = models.DateField(auto_now=True, auto_now_add=False)
