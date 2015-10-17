a5c2d3af306790f472d5ea58dd4e2b50ae1e96db
Nelson Kimaiga <nelsonkimaiga@aol.com>
2015-10-17 10:05:44 +0300

    page form data overridding with validation added

--- myApp/forms.py
+++ myApp/forms.py
@@ -1,1 +1,15 @@
 from django import forms
+from .models import Student
+
+
+class StudentForm(forms.ModelForm):
+	class Meta:
+	#	fields =  ['fullname', 'email', 'age']
+		exclude = ['last_update']
+		model = Student
+
+	def clean_age(self):
+	    age = self.cleaned_data.get('age')
+	    if age > 120:
+	        raise form.ValidationError("You may be too old for this class")
+	    return age
--- myApp/views.py
+++ myApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms imoport StudentForm
+from .forms import StudentForm
 
 # Create your views here.
 def index(request):
